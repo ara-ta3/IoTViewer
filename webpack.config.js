6d521b645823853e994fe88c5e1791a7ce2f0431
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2020-09-26 16:21:14 +0900

    Rename

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,38 @@
-const path = require('path');
+const path = require("path");
 
 const main = {
-    mode: 'development',
-    target: 'electron-main',
-    entry: path.join(__dirname, 'src', 'main'),
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, '__dist')
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
+  mode: "development",
+  target: "electron-main",
+  entry: path.join(__dirname, "src", "main"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "__dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
 
 const renderer = {
-    mode: 'development',
-    target: 'electron-renderer',
-    devtool: 'inline-source-map',
-    entry: path.join(__dirname, 'src', 'public', 'index'),
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '__dist', 'scripts')
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: [
-                    {loader: 'ts-loader'}
-                ]
-            }
-        ]
-    }
-}
+  mode: "development",
+  target: "electron-renderer",
+  devtool: "inline-source-map",
+  entry: path.join(__dirname, "src", "renderer", "index"),
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "__dist", "scripts"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: "ts-loader" }],
+      },
+    ],
+  },
+};
 
 module.exports = [main, renderer];
