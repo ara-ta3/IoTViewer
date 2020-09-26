35509683b7d4bc349879b1e2199620eb0503e908
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2020-09-26 13:36:01 +0900

    Add webpack

---
+++ src/public/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Paper, Typography, makeStyles, Theme } from '@material-ui/core';
+
+// 適用するCSSクラスの定義
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        padding: theme.spacing(3)
+    }
+}));
+
+// Headコンポーネントのインターフェースを定義
+interface HeadProps {
+    headline: string;
+}
+
+const Head: React.FC<HeadProps> = ({ headline }) => {
+    return (
+        <Typography variant="h3" component="h1">
+            {headline}
+        </Typography>
+    );
+};
+
+const App: React.FC = () => {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.root}>
+            <Head headline="Hello World" />
+        </Paper>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('contents'));
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const main = {
+    mode: 'development',
+    target: 'electron-main',
+    entry: path.join(__dirname, 'src', 'main'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, '__dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+};
+
+const renderer = {
+    mode: 'development',
+    target: 'electron-renderer',
+    devtool: 'inline-source-map',
+    entry: path.join(__dirname, 'src', 'public', 'index'),
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '__dist', 'scripts')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {loader: 'ts-loader'}
+                ]
+            }
+        ]
+    }
+}
+
+module.exports = [main, renderer];
