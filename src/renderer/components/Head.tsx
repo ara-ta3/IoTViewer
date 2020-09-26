84721ce3d7f500bc515099fa656cb5197c5ceb6f
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2020-09-26 18:20:54 +0900

    Add fetch button and fix some style

--- src/renderer/components/Head.tsx
+++ src/renderer/components/Head.tsx
@@ -1,22 +1,47 @@
 import * as React from "react";
-import { TextField, Typography } from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 export interface HeadProps {
   name: string;
   updateName: (name: string) => any;
 }
 
+const useStyles = makeStyles((theme) => ({
+  base: {
+    margin: theme.spacing(4, 0),
+  },
+}));
+
 export const Head: React.FC<HeadProps> = (props: HeadProps) => {
+  const s = useStyles();
   return (
     <div>
-      <Typography variant="h6" component="h3">
-        Hue Device Manager
-      </Typography>
-      <TextField
-        label="UserName"
-        fullWidth={true}
-        onChange={(e) => props.updateName(e.target.value)}
-      />
+      <Box className={s.base}>
+        <Typography variant="h6" component="h3">
+          Hue Device Manager
+        </Typography>
+        <TextField
+          label="UserName"
+          fullWidth={true}
+          onChange={(e) => props.updateName(e.target.value)}
+        />
+      </Box>
+      <Box>
+        <FetchButton name={props.name} />
+      </Box>
     </div>
+  );
+};
+
+const FetchButton: React.FC<{ name: string }> = ({ name }) => {
+  return name.length === 0 ? (
+    <Button variant="contained" color="primary" disabled>
+      Fetch Devices
+    </Button>
+  ) : (
+    <Button variant="contained" color="primary">
+      Fetch Devices
+    </Button>
   );
 };
