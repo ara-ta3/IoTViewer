a0b785f76d928208ff81fc5aa61ab5ba3039376a
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2021-01-30 15:03:01 +0900

    Remove gateway class and separate to functions

--- src/HueGateway.ts
+++ src/HueGateway.ts
@@ -2,32 +2,9 @@ import fetch from "node-fetch";
 import { Device } from "./Contract";
 import { fromNullable, map, Option } from "fp-ts/Option";
 
-class HueGateway {
-  readonly apiEndpoint: string;
-  readonly userName: string;
-  constructor(apiEndpoint: string, userName: string) {
-    this.apiEndpoint = apiEndpoint;
-    this.userName = userName;
-  }
-
-  async fetchDevices(): Promise<DevicesResponse> {
-    const url = `${this.apiEndpoint}/api/${this.userName}/lights`;
-    const res = await fetch(url);
-    return res.json();
-  }
-}
-
 interface DiscoveryMeethueResponseValue {
   id: string;
   internalipaddress: string;
-}
-
-export async function fetchHueApiEndpoint(): Promise<Option<string>> {
-  const res = await fetch("https://discovery.meethue.com/");
-  const json: DiscoveryMeethueResponseValue[] = await res.json();
-  return map((x: DiscoveryMeethueResponseValue) => x.internalipaddress)(
-    fromNullable(json.pop())
-  );
 }
 
 interface DevicesResponse {
@@ -48,12 +25,21 @@ function responseToDevices(res: DevicesResponse): Device[] {
   });
 }
 
+export async function fetchHueApiEndpoint(): Promise<Option<string>> {
+  const res = await fetch("https://discovery.meethue.com/");
+  const json: DiscoveryMeethueResponseValue[] = await res.json();
+  return map((x: DiscoveryMeethueResponseValue) => x.internalipaddress)(
+    fromNullable(json.pop())
+  );
+}
+
 export async function fetchDevices(
   apiEndpoint: string,
   userName: string
 ): Promise<Device[]> {
-  const g = new HueGateway(apiEndpoint, userName);
-  const res = await g.fetchDevices();
-  console.log(res);
-  return responseToDevices(res);
+  const url = `${apiEndpoint}/api/${userName}/lights`;
+  const res = await fetch(url);
+  const json: DevicesResponse = await res.json();
+  console.log(json);
+  return responseToDevices(json);
 }
